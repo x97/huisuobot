75e3231f9293ed7d2cb6d12b3e50951676ea82c0
x97 <progress_wang@163.com>
2026-01-06 12:41:03 +0800

    群里查询报告

--- reports/handlers/report_query.py
+++ reports/handlers/report_query.py
@@ -149,7 +149,7 @@ def report_pagination_callback(update: Update, context: CallbackContext):
 def register_report_handlers(dp):
     # 只匹配以“报告#”开头的消息
     dp.add_handler(MessageHandler(
-        Filters.regex(r"^报告#.+"),
+        Filters.regex(r"^报告#.+") & Filters.chat_type.groups,
         report_query_handler
     ))
 
